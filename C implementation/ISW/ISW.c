2fab6ddedeeca3649b1dd0087559618b5f8f7b60
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-03-29 16:41:20 +0200

    Update ISW.c

--- C implementation/ISW/ISW.c
+++ C implementation/ISW/ISW.c
@@ -1,31 +1,70 @@
-#include "ISW.h"
 #include "stdint.h"
-#include "gfmul.h"
+#include "stdlib.h" //rand()
+#include "ISW.h"
+
+static uint8_t a[Mask_ORD+1];
+static uint8_t b[Mask_ORD+1];
 
 
-
-void ISW_MULT(uint8_t* a, uint8_t* b, uint8_t* c)
+uint8_t gfMul(uint8_t a, uint8_t b)
 {
-    uint8_t r[NUM_SHARES][NUM_SHARES];
-    for (int i = 0; i < NUM_SHARES; i++)
+    int s = 0;
+    s = table[a] + table[b];
+    int q;
+    /* Get the antilog */
+    s = table[s+256];
+    uint8_t z = 0;
+    q = s;
+    if(a == 0) {
+        s = z;
+    } else {
+        s = q;
+    }
+    if(b == 0) {
+        s = z;
+    } else {
+        q = z;
+    }
+    return s;
+}
+
+
+    void Mask(uint8_t y[Mask_ORD+1], uint8_t* x)
     {
-        for (int j = i + 1; j < NUM_SHARES; j++)
+        y[0] = x;
+        for(int i = 1; i <= Mask_ORD; i++)
         {
-             r[i][j]= getRand();
-            // The order is important
-             r[j][i] = (r[i][j] ^ gfMul(a[i], b[j])) ^ gfMul(a[j], b[i]);
+            y[i]=  rand() % 256;
+            y[0] = y[0] ^ y[i];
         }
     }
-    for (int i = 0; i < NUM_SHARES; i++)
+
+
+    void Seminal_ISW(uint8_t* input_a, uint8_t* input_b, uint8_t* c)
     {
-        c[i]= gfMul(a[i], b[i]);
-        for (int j = 0; j < NUM_SHARES; j++)
+        Mask(a, input_a);
+        Mask(b, input_b);
+        int i, j;
+        uint8_t r[Mask_ORD+1][Mask_ORD+1];
+        for (i = 0; i < Mask_ORD+1; i++)
         {
-            if (i != j)
+            for (j = i + 1; j < Mask_ORD+1; j++)
             {
-                c[i] ^=r[i][j];
+                r[i][j]= rand() % 256;
+                // The order is important
+                r[j][i] = (r[i][j] ^ gfMul(a[i], b[j])) ^ gfMul(a[j], b[i]);
             }
         }
+        for (i = 0; i < Mask_ORD+1; i++)
+        {
+            c[i]= gfMul(a[i], b[i]);
+            for (j = 0; j < Mask_ORD+1; j++)
+            {
+                if (i != j)
+                {
+                    c[i] ^=r[i][j];
+                }
+            }
 
+        }
     }
-}
