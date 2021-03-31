c9ccd8d434aeba7b76f28f46585714d1fcd4f208
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-03-31 14:06:49 +0200

    Add files via upload

---
+++ C implementation/ISW_physical_defaults/ISW_G.c
@@ -0,0 +1,73 @@
+#include "stdint.h"
+#include "stdlib.h" //rand()
+#include "ISW_G.h"
+
+static uint8_t a[Mask_ORD+1];
+static uint8_t b[Mask_ORD+1];
+
+
+uint8_t gfMul(uint8_t a, uint8_t b)
+{
+    int s = 0;
+    s = table[a] + table[b];
+    int q;
+    /* Get the antilog */
+    s = table[s+256];
+    uint8_t z = 0;
+    q = s;
+    if(a == 0) {
+        s = z;
+    } else {
+        s = q;
+    }
+    if(b == 0) {
+        s = z;
+    } else {
+        q = z;
+    }
+    return s;
+}
+
+
+    void Mask(uint8_t y[Mask_ORD+1], uint8_t* x)
+    {
+        y[0] = x;
+        for(int i = 1; i <= Mask_ORD; i++)
+        {
+            y[i]=  rand() % 256;
+            y[0] = y[0] ^ y[i];
+        }
+    }
+
+
+    void Seminal_ISW(uint8_t* input_a, uint8_t* input_b, uint8_t* c)
+    {
+        Mask(a, input_a);
+        Mask(b, input_b);
+        int i, j;
+        uint8_t r[Mask_ORD+1][Mask_ORD+1];
+        uint8_t u[Mask_ORD+1][Mask_ORD+1];
+
+        for (i = 0; i < Mask_ORD+1; i++)
+        {
+            for (j = i + 1; j < Mask_ORD+1; j++)
+            {
+                r[i][j]= rand() % 256;
+                // The order is important
+                u[i][j] = (r[i][j] ^ gfMul(a[i], b[j]));
+                u[j][i] = (r[i][j] ^ gfMul(a[j], b[i]));
+            }
+        }
+        for (i = 0; i < Mask_ORD+1; i++)
+        {
+            c[i]= gfMul(a[i], b[i]);
+            for (j = 0; j < Mask_ORD+1; j++)
+            {
+                if (i != j)
+                {
+                    c[i] ^=u[i][j];
+                }
+            }
+
+        }
+    }
