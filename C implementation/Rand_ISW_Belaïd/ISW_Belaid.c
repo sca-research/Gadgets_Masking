4c61d01f2b44236f2ed1f01863c7ff34f869d36c
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-03-31 15:44:37 +0200

    Update ISW_Belaid.c
    r

--- C implementation/Rand_ISW_Belaïd/ISW_Belaid.c
+++ C implementation/Rand_ISW_Belaïd/ISW_Belaid.c
@@ -37,11 +37,23 @@ uint8_t gfMul(uint8_t a, uint8_t b)
         }
     }
 
+    void Refresh_Mask(uint8_t y[Mask_ORD+1], uint8_t x[Mask_ORD+1])
+    {
+        uint8_t tmp;
+        for (int i = 1; i <= Mask_ORD; i++) {
+            tmp = rand() % 256;
+            y[0] = x[0] ^ tmp;
+            y[i] = x[i] ^ tmp;
+        }
+    }
+
 
     void ISW_Belaid_MULT(uint8_t* input_a, uint8_t* input_b, uint8_t* c)
     {
         Mask(a, input_a);
         Mask(b, input_b);
+        Refresh_Mask(a,a);
+        Refresh_Mask(b,b);
         int i, j;
         uint8_t r2[Mask_ORD+1][Mask_ORD+1];
         uint8_t r1[Mask_ORD+1];
