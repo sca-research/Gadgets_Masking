f377f9af5c0ab71fd497e17f1827dbe4d97abb54
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-03-31 16:36:05 +0200

    Update ISW.c

--- C implementation/ISW/ISW.c
+++ C implementation/ISW/ISW.c
@@ -39,26 +39,15 @@ uint8_t gfMul(uint8_t a, uint8_t b)
     }
 
 
-    void Refresh_Mask(uint8_t y[Mask_ORD+1], uint8_t x[Mask_ORD+1])
-    {
-        uint8_t tmp;
-        for(int i=1; i<= Mask_ORD; i++)
-        {
-            tmp = rand() %256;
-            y[0] = x[0] ^ tmp;
-            y[i] = x[i] ^ tmp;
-        }
-    }
-
 
     void Seminal_ISW(uint8_t* input_a, uint8_t* input_b, uint8_t* c)
     {
         Mask(a, input_a);
         Mask(b, input_b);
-        Refresh_Mask(a,a);
-        Refresh_Mask(b,b);
+        
         int i, j;
         uint8_t r[Mask_ORD+1][Mask_ORD+1];
+        
         for (i = 0; i < Mask_ORD+1; i++)
         {
             for (j = i + 1; j < Mask_ORD+1; j++)
