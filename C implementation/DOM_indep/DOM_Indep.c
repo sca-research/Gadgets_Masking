5d891a86755f486d6f5c2035a1fa32f938d38e12
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-04-06 19:52:29 +0200

    Update DOM_Indep.c
    edit

--- C implementation/DOM_indep/DOM_Indep.c
+++ C implementation/DOM_indep/DOM_Indep.c
@@ -50,7 +50,7 @@ static uint8_t b[Mask_ORD+1];
         int i, j;
         int all_terms = Mask_ORD * (Mask_ORD+1)/2;
         uint8_t r[all_terms];
-        uint8_t reg[all_terms];
+        uint8_t reg[2*all_terms];
 
         for (i = 0; i < all_terms; i++){
             r [i]= rand() % 256;
@@ -59,7 +59,9 @@ static uint8_t b[Mask_ORD+1];
                 uint8_t result = 0;
                 for (j = 0; j < Mask_ORD + 1; j++){
                     int p = (Mask_ORD + 1) * i + j;
-
+                    if (i == j){
+                        result = result ^  gfMul(a[i],b[j]);
+                    }
                     if (j > i){
                         reg[p] = (gfMul(a[i], b[j])) ^ r[i + (j*(j-1)/2)];
                     }
@@ -71,7 +73,7 @@ static uint8_t b[Mask_ORD+1];
                         result = result ^ reg[p];
                     }
                 }
-                c[i] = gfMul(a[i],b[i]) ^ result;
+                c[i] = result;
             }
 
     }
