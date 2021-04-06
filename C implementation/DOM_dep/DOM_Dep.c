3a4b32038276b12af8d1a67e71ecdbd47e1f4785
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-04-06 20:48:25 +0200

    Update DOM_Dep.c
    edit

--- C implementation/DOM_dep/DOM_Dep.c
+++ C implementation/DOM_dep/DOM_Dep.c
@@ -1,3 +1,4 @@
+//DOM-dep
 #include "stdint.h"
 #include "stdlib.h" //rand()
 #include "DOM_Dep.h"
@@ -28,11 +29,9 @@ static uint8_t b[Mask_ORD+1];
     }
 
 
-    void Mask(uint8_t y[Mask_ORD+1], uint8_t* x)
-    {
+    void Mask(uint8_t y[Mask_ORD+1], uint8_t* x){
         y[0] = x;
-        for(int i = 1; i <= Mask_ORD; i++)
-        {
+        for(int i = 1; i <= Mask_ORD; i++){
             y[i]=  rand() % 256;
             y[0] = y[0] ^ y[i];
         }
@@ -40,59 +39,45 @@ static uint8_t b[Mask_ORD+1];
 
     void sum_vect(uint8_t in_vect[Mask_ORD+1], uint8_t sum)
     {
-        for(int i=1; i< Mask_ORD+1; i++)
-        {
+        for(int i=1; i< Mask_ORD+1; i++){
             sum += in_vect[i];
         }
     }
 
-    void DOM_independent(uint8_t* input_a, uint8_t* input_b, uint8_t* c)
-    {
+    void DOM_independent(uint8_t* input_a, uint8_t* input_b, uint8_t* c){
 
-        int i, j, k;
+        int i, j;
+        int all_terms = Mask_ORD * (Mask_ORD+1)/2;
+        uint8_t r[all_terms];
+        uint8_t reg[2*all_terms];
 
-        if (Mask_ORD % 2 == 0)
-        {
-            k = j;
-        }
-        else
-        {
-            k = (i + j) % 2;
-        }
-
-        int randoms = Mask_ORD * (Mask_ORD+1)/2;
-        uint8_t r[randoms];
-        uint8_t cross_domain[randoms];
-
-        for (i = 0; i < randoms; i++)
-        {
+        for (i = 0; i < all_terms; i++){
             r [i]= rand() % 256;
         }
-
-        for (i = 0; i < Mask_ORD+1; i++) {
-            for (j = 0; j < Mask_ORD + 1; j++) {
+        for (i = 0; i < Mask_ORD + 1; i++){
+            uint8_t result = 0;
+            for (j = 0; j < Mask_ORD + 1; j++){
                 int p = (Mask_ORD + 1) * i + j;
-                if (i != j) {
-                    cross_domain[p] = (gfMul(input_a[i], input_b[j])) ^ r[k];
+                if (i == j){
+                    result = result ^  gfMul(input_a[i],input_b[j]);
+                }
+                if (j > i){
+                    reg[p] = (gfMul(input_a[i], input_b[j])) ^ r[i + (j*(j-1)/2)];
+                }
+                else{
+                    reg[p] = (gfMul(input_a[i], input_b[j])) ^ r[j + (i*(i-1)/2)];
+                }
+                if (i!=j){
+
+                    result = result ^ reg[p];
                 }
             }
+            c[i] = result;
         }
 
-        for (i = 0; i < Mask_ORD+1; i++)
-        {
-            c[i]= gfMul(input_a[i], input_b[i]);
-
-            for (j = 0; j < Mask_ORD+1; j++)
-            {
-                int p = (Mask_ORD + 1) * i + j;
-                if (i != j)
-                {
-                    c[i] ^=cross_domain[p];
-                }
-            }
-
-        }
     }
+
+
 
 
     void DOM_dependent(uint8_t* input_a, uint8_t* input_b, uint8_t* c)
@@ -106,8 +91,7 @@ static uint8_t b[Mask_ORD+1];
         uint8_t z[Mask_ORD+1];
         uint8_t x[Mask_ORD+1];
 
-        for (i = 0; i < Mask_ORD+1; i++)
-        {
+        for (i = 0; i < Mask_ORD+1; i++){
             z[i] = rand() % 256;
             x[i] = b[i] ^ z[i];
         }
@@ -115,8 +99,7 @@ static uint8_t b[Mask_ORD+1];
         sum_vect(x, x_sum);
         DOM_independent(a, b, out_Dom_ind);
 
-        for (i = 0; i < Mask_ORD+1; i++)
-        {
+        for (i = 0; i < Mask_ORD+1; i++){
             c[i] = out_Dom_ind[i] ^ (gfMul(a[i] , x_sum));
         }
 
