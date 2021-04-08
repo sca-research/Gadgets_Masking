5cb8805637b20dd233dcf72c7f455c2a8b4f81ea
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-04-08 11:28:21 +0200

    Update ISW_Belaid.c

--- C implementation/Rand_ISW_Belaïd/ISW_Belaid.c
+++ C implementation/Rand_ISW_Belaïd/ISW_Belaid.c
@@ -1,3 +1,7 @@
+/*
+Paper: Randomness Complexity of Private Circuits for Multiplication, Algorithm 3
+*/
+
 #include "stdint.h"
 #include "stdlib.h" //rand()
 #include "ISW_Belaid.h"
@@ -28,12 +32,10 @@ static uint8_t b[Mask_ORD+1];
     }
 
 
-    void Mask(uint8_t y[Mask_ORD+1], uint8_t* x)
-    {
+    void Mask(uint8_t y[Mask_ORD+1], uint8_t* x){
         y[0] = x;
-        
-        for(int i = 1; i <= Mask_ORD; i++)
-        {
+
+        for(int i = 1; i <= Mask_ORD; i++){
             y[i]=  rand() % 256;
             y[0] = y[0] ^ y[i];
         }
@@ -46,52 +48,42 @@ static uint8_t b[Mask_ORD+1];
         Mask(b, input_b);
 
         int i, j;
-        
+
         uint8_t r2[Mask_ORD+1][Mask_ORD+1];
         uint8_t r1[Mask_ORD+1];
         uint8_t temp[Mask_ORD+1][Mask_ORD+1];
 
-        for (i = 0; i <= Mask_ORD; i++)
-        {
-            for (j = 0; j <= Mask_ORD-i-1; j+=2)
-            {
+        for (i = 0; i <= Mask_ORD; i++){
+            for (j = 0; j <= Mask_ORD-i-1; j+=2){
                 r2[i][Mask_ORD-j] = rand() % 256;
             }
         }
-        
-        for (j = Mask_ORD-1; j >= 1; j-=2)
-        {
+
+        for (j = Mask_ORD-1; j >= 1; j-=2){
             r1[j] = rand() % 256;
         }
-        
-        for (i = 0; i <=Mask_ORD; i++)
-        {
+
+        for (i = 0; i <=Mask_ORD; i++){
             c[i] = gfMul(a[i], b[i]);
-            
-            for (j = Mask_ORD; j >= i + 2; j-=2)
-            {
+
+            for (j = Mask_ORD; j >= i + 2; j-=2){
                 temp[i][j] = r2[i][j] ^ (gfMul(a[i], b[j])) ^ (gfMul(a[j], b[i]))^
                             r1[j-1] ^ (gfMul(a[i], b[j-1])) ^ (gfMul(a[j-1], b[i]));
                 c[i] ^= temp[i][j];
             }
-            
-            if (!((i%2)&((Mask_ORD)%2))) //(i%2) != (NUM_SHARES%2)
-            {
+
+            if ((i%2) != (Mask_ORD%2)) {
                 temp[i][i+1] = r2[i][i+1] ^ (gfMul(a[i], b[i+1])) ^ (gfMul(a[i+1], b[i]));
                 c[i] ^=temp[i][i+1];
-                
-                if ((i%2)&1)
-                {
+
+                if ((i%2)&1){
                     c[i] ^= r1[i];
                 }
             }
-            
-            else
-            {
-                for (j = i-1; j >= 0; j--)
-                {
-                    c[i] ^= r2[j][i];
 
+            else{
+                for (j = i-1; j >= 0; j--){
+                    c[i] ^= r2[j][i];
                 }
             }
 
