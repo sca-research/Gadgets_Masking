800c5827c4dda494285fdaaa7f4006737c3951d5
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-04-21 09:14:49 +0200

    Update Ref_Mask.c

--- C implementation/HPC_1/RefreshMasks/Ref_Mask.c
+++ C implementation/HPC_1/RefreshMasks/Ref_Mask.c
@@ -5,16 +5,18 @@
 int count_rnd =0;
 
 //////////////////////////////////////////////////////////////////////
-
 uint8_t refresh_mask(uint8_t* a, int index_start, int index_end, uint8_t* rnd, uint8_t* d){
+    
     static uint8_t b[Mask_ORD+1];
     static uint8_t c[Mask_ORD+1];
 
     int n = index_end - index_start+1;
+    
     if (n == 1){
         d[index_start] = a[index_start];
         return d[index_start];
     }
+    
     if (n == 2){
         d[index_start] = a[index_start] ^ rnd[count_rnd];
         d[index_start+1] = a[index_start+1] ^ rnd[count_rnd];
@@ -23,13 +25,12 @@ uint8_t refresh_mask(uint8_t* a, int index_start, int index_end, uint8_t* rnd, u
     }
 
     for(int i = 0; i < n/2; i++){
-
         b[i+index_start] = a[i+index_start] ^ rnd[count_rnd];
         int j = (int)(n/2)+i+index_start;
         b[j] = a[j] ^ rnd[count_rnd];
         count_rnd++;
     }
-    int y = n-1;
+    
     if ((n % 2) == 1){
         b[n-1] = a[n-1];
     }
@@ -44,8 +45,8 @@ uint8_t refresh_mask(uint8_t* a, int index_start, int index_end, uint8_t* rnd, u
         d[j] = c[j] ^ rnd[count_rnd];
         count_rnd++;
     }
+    
     if ((n % 2) == 1){
         d[n-1] = c[n-1];
     }
-    printf("\nrnd %d", count_rnd);
 }
