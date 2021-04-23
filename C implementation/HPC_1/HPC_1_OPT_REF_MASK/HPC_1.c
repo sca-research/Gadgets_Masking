976fa04d2885c2151a72fe6b804144825743a05d
pamahdion <parinaz.mahdion@aau.at>
2021-04-23 17:34:33 +0200

    add

---
+++ C implementation/HPC_1/HPC_1_OPT_REF_MASK/HPC_1.c
@@ -0,0 +1,47 @@
+
+/*
+ HPC_1: Fig.2 (c)  in "Hardware Private Circuits: From Trivial Composition to Full Verification"
+*/
+#include "basic_math.h"
+#include "HPC_1.h"
+#include "Opt_Ref_Mask.h"
+#include "DOM_Indep.h"
+#include "string.h" // memcpy func
+/*
+    hpc1(INPUT: input_a[Mask_ORD+1], INPUT: input_b[Mask_ORD+1], INPUT: uint8_t* rnd_Ref,INPUT: uint8_t * rnd_DOM, OUTPUT: c[Mask_ORD+1])
+    c = a * b
+    * works for any field : this program is for field: 2^3=8
+    *
+        rnd_DOM: Mask_ORD * (Mask_ORD+1) /2;
+        rnd_DOM: Func --> rnd_number(int Mask_order);
+        */
+
+////////////////////////////////////////////////
+
+ void hpc1(uint8_t* input_a, uint8_t* input_b, uint8_t* rnd_Ref, uint8_t * rnd_DOM, uint8_t* c) {
+    int i, j;
+
+    static uint8_t share_0_mask[Mask_ORD + 1]; // The output of opt_refresh_mask
+    static uint8_t add_inb_share0[Mask_ORD + 1]; // Adding input_b and share_0_mask
+    static uint8_t input_b_Ref[Mask_ORD + 1]; // Register for the output of adding input_b and share_0_mask
+
+
+    /*Refresh part:*/
+    ////////////////////////////////////////////////////////////////////
+    uint8_t all_zero_input[Mask_ORD+1] = {0x00};
+
+    opt_refresh_mask(all_zero_input, rnd_Ref, share_0_mask);
+
+    for (int i = 0; i <= Mask_ORD; i++) {
+        add_inb_share0[i] = input_b[i] ^ share_0_mask[i];
+    }
+    memcpy(input_b_Ref, add_inb_share0, sizeof add_inb_share0);
+
+
+    /*Multiplication part:*/
+    ////////////////////////////////////////////////////////////////////
+    DOM_independent(input_a, input_b_Ref, rnd_DOM, c);
+}
+
+
+
---
+++ C implementation/HPC_1/HPC_1_OPT_REF_MASK/basic_math.c
@@ -0,0 +1,56 @@
+#include <basic_math.h>
+
+
+void Mask(uint8_t* y, uint8_t x)
+{
+    y[0] = x;
+    for(int i = 1; i <= Mask_ORD; i++)
+    {
+        y[i]=  rand() % 256;
+        y[0] = y[0] ^ y[i];
+    }
+}
+
+
+uint8_t gfMul(uint8_t a, uint8_t b)
+{
+    int s = 0;
+    s = table[a] + table[b];
+    int q;
+    /* Get the antilog */
+    s = table[s+256];
+    uint8_t z = 0;
+    q = s;
+    if(a == 0) {
+        s = z;
+    } else {
+        s = q;
+    }
+    if(b == 0) {
+        s = z;
+    } else {
+        q = z;
+    }
+    return s;
+}
+
+
+// The number of randomness in Optimized RefreshMask
+int rnd_number(int Mask_order) {
+    int d = Mask_order + 1;
+    if (d <= 3) {
+        return (d - 1);
+    }
+    if (d <= 5) {
+        return d;
+    }
+    if (d <= 11) {
+        return (2 * d - 5);
+    }
+    if (d == 12) {
+        return (d + 8);
+    }
+    if (d <= 16) {
+        return (2 * d);
+    }
+}
