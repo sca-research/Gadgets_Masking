a35fd4f453a9e78f6141b46d3b92b98b65c8cfca
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-05-10 16:17:27 +0200

    Update basic_math.c

--- C implementation/HPC_1/HPC_1_REF_MASK/basic_math.c
+++ C implementation/HPC_1/HPC_1_REF_MASK/basic_math.c
@@ -16,22 +16,37 @@ uint8_t gfMul(uint8_t a, uint8_t b)
 {
     int s = 0;
     s = table[a] + table[b];
-    int q;
     /* Get the antilog */
     s = table[s+256];
-    uint8_t z = 0;
-    q = s;
-    if(a == 0) {
-        s = z;
-    } else {
-        s = q;
-    }
-    if(b == 0) {
-        s = z;
-    } else {
-        q = z;
-    }
+/*
+    Checking a=0 or b=0, without conditional branch: if (a==0 or b==0){return 0;} else{return s;}
+     Countermeasure for Power analysis attacks
+*/
+    uint8_t tmp = 0;
+    tmp = b & (-a >> 8);
+    s = s & (-tmp >> 8);
     return s;
 }
 
+/* uint8_t gfMul(uint8_t a, uint8_t b)
+   {
+       int s = 0;
+       s = table[a] + table[b];
+       int q;
+       *//* Get the antilog *//*
+        s = table[s+256];
+        uint8_t z = 0;
+        q = s;
+        if(a == 0) {
+            s = z;
+        } else {
+            s = q;
+        }
+        if(b == 0) {
+            s = z;
+        } else {
+            q = z;
+        }
+        return s;
+    }*/
 
