db49fac6855273bdc7683c01d2993d3ff016451a
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-06-10 18:38:53 +0200

    Update main.c

--- C implementation/DOM_dep/main.c
+++ C implementation/DOM_dep/main.c
@@ -6,6 +6,8 @@
 #include "DOM_Dep.h"
 
 uint8_t gmul(uint8_t a, uint8_t b);
+void Mask(uint8_t x, uint8_t* rnd, uint8_t* y);
+int rnd_n = (Mask_ORD+1) + (Mask_ORD * (Mask_ORD+1) /2); // The number of randomness in DOM_dep multiplication gadget
 
 //////////////////////////////////////////////////////////////////////
 int main(void)
@@ -13,35 +15,71 @@ int main(void)
     time_t t;
     srand((unsigned) time(&t));
 
-    for (int i = 0; i < 1000; i++) {
-        uint8_t a = rand() % 256;
-        uint8_t b = rand() % 256;
-        uint8_t c[Mask_ORD + 1];
+    for (int i = 0; i < 100; i++)
+    {
+        // Inputs
+        uint8_t input_a = rand() % 256;
+        uint8_t input_b = rand() % 256;
 
-        DOM_dependent(a, b, c);
+        // Input shares of the Gadget (a=a_0,a_1,...,a_d and b=b_0,b_1,...,b_d)
+        static uint8_t a[Mask_ORD+1];
+        static uint8_t b[Mask_ORD+1];
 
+        // Output of the Gadget is c_0, c_1, ..., c_d such that c_0 + c_1 + ... + c_d = c (c=a*b)
+        static uint8_t c[Mask_ORD + 1];
 
-        // printf("\n  -------------------------Number of shares: %0d", Mask_ORD+1);
-/*        for (int i = 0; i <= Mask_ORD; i++) {
-            printf(" %02x ", c[i]);
-        }*/
+        static uint8_t rnd_a[Mask_ORD]; // Random number for masking input a
+        static uint8_t rnd_b[Mask_ORD]; // Random number for masking input b
+        static uint8_t rnd_Dom_dep[(Mask_ORD+1) + (Mask_ORD * (Mask_ORD+1) /2)]; // Random number for Dom_dep multiplication
+
+        for (int k = 0; k <Mask_ORD; k++)
+        {
+            rnd_a[k] = rand() % 256;
+            rnd_b[k] = rand() % 256;
+        }
+
+        for (int k = 0; k <rnd_n; k++)
+        {
+            rnd_Dom_dep[k] = rand() % 256;
+        }
+
+        // Input shares of the Gadget (a=a_0,a_1,...,a_d and b=b_0,b_1,...,b_d)
+        Mask(input_a, rnd_a, a);
+        Mask(input_b, rnd_b, b);
+
+        /*
+        DOM_dependent(INPUT: a[Mask_ORD+1], INPUT: b[Mask_ORD+1], INPUT: rnd[(Mask_ORD+1) + (Mask_ORD * (Mask_ORD+1) /2)], OUTPUT: c[Mask_ORD])
+        c = a * b
+        rnd_Dom_dep: random numbers (on_the_fly)*/
+        DOM_dependent(a, b, rnd_Dom_dep, c);
+
+        // Verifying the Gadget
+        // Comparing the output of the Gadget with the unmasked multiplication
         uint8_t output = 0;
-        for (int i = 0; i <= Mask_ORD; i++) {
+        for (int i = 0; i <= Mask_ORD; i++)
+        {
             output ^= c[i];
         }
-/*
-        printf(" \n a: %02x  \n b: %02x", a, b );
-        printf(" \n Unmasked_c = a * b: %02x \n     Mask_c = a * b: %02x ",gfMul(a,b) , output);
-*/
 
-        if (output != gmul(a, b)) {
-//            printf(" \n Error for inputs : a = %02x , b = %02x and Num_shares: %0d \n", a, b, Mask_ORD+1);
-            printf(" \n Error for Num_shares: %0d \n",  Mask_ORD+1);
+        printf("\n____________________________________________________________");
+        printf(" \n a: %02x  \n b: %02x", input_a, input_b );
+        printf(" \nUnmasked_c = a * b: %02x\nMask_c = a * b: %02x ",gfMul(input_a,input_b) , output);
 
+        if (output != gmul(input_a, input_b))
+        {
+            printf(" \n Error: a = %02x , b = %02x and Num_shares: %0d \n", input_a, input_b, Mask_ORD+1);
+            break;
         }
-        else{
-            printf("  CORRECT  ");
-        }
+    }
+}
+
+void Mask(uint8_t x, uint8_t* rnd, uint8_t* y)
+{
+    y[Mask_ORD] = x;
+    for(int i = 0; i < Mask_ORD; i++)
+    {
+        y[i]=  rnd[i];
+        y[Mask_ORD] ^= y[i];
     }
 }
 
@@ -50,9 +88,11 @@ int main(void)
 Multiplication of two numbers (a and b) in the GF(2^8) with the polynomial x^8 + x^4 + x^3 + x + 1
 x^8 + x^4 + x^3 + x + 1---> in binary format:10001011 = 11b
 */
-uint8_t gmul(uint8_t a, uint8_t b) {
+uint8_t gmul(uint8_t a, uint8_t b)
+{
     uint8_t p = 0; /* the product of the multiplication */
-    while (a && b) {
+    while (a && b)
+    {
         if (b & 1) /* if b is odd, then add the corresponding a to p (final product = sum of all a's corresponding to odd b's) */
             p ^= a; /* since we're in GF(2^m), addition is an XOR */
 
@@ -64,4 +104,3 @@ uint8_t gmul(uint8_t a, uint8_t b) {
     }
     return p;
 }
-
