c2cc3eeebb10288fe43cef7beb5aef207bace95e
Nima <67910202+mahdion-aau@users.noreply.github.com>
2021-06-23 15:29:40 +0200

    Add files via upload

---
+++ Assembly_implementation/run.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# ./run in terminal: Permission denied, solving: chmod +x ./run.sh, and then for running: ./run
+
+# variable: name of files
+file=${1:-ISW_1}     #first argument
+project_=${2:-isw_1} #second agument   
+
+#then for running: ./run.sh ISW_1 isw_1
+
+#echo "The current directory is:"
+pwd
+export SCALE="${PWD}"
+cd hw
+pwd
+ls
+printf '\n'
+export SCALE_HW="${PWD}"
+export TARGET="${SCALE_HW}/target/lpc1313fbd48"
+cd ${TARGET}
+make --no-builtin-rules clean all
+cd ${SCALE_HW}
+cd $file
+pwd
+ls
+printf '\n'
+sudo make --no-builtin-rules -f ${TARGET}/build/lib/scale.mk BSP="${TARGET}/build" USB="/dev/ttyUSB0" PROJECT="$project_" PROJECT_SOURCES="$project_.c $project_.S" clean all program
